c3ce5c0266fcf80fb8cfd757df9f7a0b0ec0ea02
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2025-03-20 12:26:48 +0300

    1

--- exec-tests.sh
+++ exec-tests.sh
@@ -1,6 +1,6 @@
 export ALLURE_TOKEN=$(cat ../secrets/xyz-token.txt)
 export ALLURE_ENDPOINT=$(cat ../secrets/xyz-endpoint.txt)
-export ALLURE_PROJECT_ID=1
+export ALLURE_PROJECT_ID=1605
 export ALLURE_LAUNCH_NAME="checking various attachments"
 export ALLURE_RESULTS=allure-results
 
