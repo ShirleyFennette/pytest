e41e2421f96264965d5a4e6055cf15dbd6a08cd1
ShirleyFennette <dkopylov@qatools.ru>
2025-03-20 12:57:16 +0300

    1

--- exec-tests.sh
+++ exec-tests.sh
@@ -6,3 +6,4 @@ export ALLURE_RESULTS=allure-results
 
 ./allurectl watch -- pytest ./tests --alluredir=${ALLURE_RESULTS}
 --endpoint http://testing.qatools.cloud
+--token 67aad6fa-966a-49ef-a598-e572b3e030f9
