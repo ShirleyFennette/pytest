86e7217f44fcd5f3f5a7011c7c512e969885cb85
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2025-06-10 14:58:15 +0300

    Update exec-tests.sh

--- exec-tests.sh
+++ exec-tests.sh
@@ -1,6 +1,6 @@
 export ALLURE_TOKEN=67aad6fa-966a-49ef-a598-e572b3e030f9
 export ALLURE_ENDPOINT=http://testing.qatools.cloud
-export ALLURE_PROJECT_ID=1605
+export ALLURE_PROJECT_ID=1703
 export ALLURE_LAUNCH_NAME="checking various attachments"
 export ALLURE_RESULTS=allure-results
 
