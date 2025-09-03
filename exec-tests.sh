b6c3d1fb5fb6c35b8bb4b433dc6bd424dac356fb
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2025-09-03 14:01:53 +0300

    Update exec-tests.sh

--- exec-tests.sh
+++ exec-tests.sh
@@ -3,6 +3,7 @@ export ALLURE_ENDPOINT=http://testing.qatools.cloud
 export ALLURE_PROJECT_ID=1605
 export ALLURE_LAUNCH_NAME="checking various attachments"
 export ALLURE_RESULTS=allure-results
+export ALLURE_LAUNCH_TAGS="release"
 
 ./allurectl watch -- pytest ./tests --alluredir=${ALLURE_RESULTS}
 --endpoint http://testing.qatools.cloud
